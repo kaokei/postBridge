4976ea74975c77771bb420dfd0daeae385c8f818
kaokei <kaokei.rst@gmail.com>
2021-04-27 01:15:53 +0800

    完善README.md

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
-  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-  },
+  moduleFileExtensions: ['js', 'ts', 'json'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^react-native$': 'react-native-web',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-  },
-  globals: {
-    'vue-jest': {
-      babelConfig: './babel.config.js',
-    },
   },
 };
